378c5c9b955f76e2444aba5681d357edb4e91eba
dennomwas <githinji.mwangi@gmail.com>
2018-09-24 09:33:08 +0300

    [Ft] Create Node server

---
+++ app.js
@@ -0,0 +1,9 @@
+let http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content - Type': 'text / plain'
+    });
+    response.end("Hello World\n");
+}).listen(1337, "127.0.0.1");
+console.log("Server running at http://127.0.0.1:1337/")
